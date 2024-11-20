b43ccab4489358d2517a208fc07aa1eee9a5a847
arielglvz <arielglvz@gmail.com>
2024-11-20 19:22:47 +0800

    Vue3: Dynamic Links

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Jobs from '../views/jobs/Jobs.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/jobs',
+    name: 'jobs',
+    component: Jobs,
+  },
+  
+  {
+    path: '/jobs/:id',
+    name: 'JobDetails',
+    component: JobDetails,
+    props: true
+  }, 
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, neque earum esse molestiae cumque culpa! Cupiditate distinctio ipsum maxime rem veniam culpa, eius error eveniet architecto nam laudantium aspernatur quisquam consequatur explicabo eligendi, animi veritatis quibusdam similique ad ipsam nesciunt nulla amet. Culpa similique sed, iste animi error ea. At assumenda facere doloribus esse ratione nulla tenetur commodi et illo! Doloremque labore expedita dolorem aliquam animi magni est minima, laudantium temporibus placeat facilis recusandae consequatur asperiores eaque ratione fugit cumque iure reiciendis nostrum. Qui totam magni quae saepe. Similique optio aut officiis voluptatem perferendis odio numquam, modi ipsa autem laboriosam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, neque earum esse molestiae cumque culpa! Cupiditate distinctio ipsum maxime rem veniam culpa, eius error eveniet architecto nam laudantium aspernatur quisquam consequatur explicabo eligendi, animi veritatis quibusdam similique ad ipsam nesciunt nulla amet. Culpa similique sed, iste animi error ea. At assumenda facere doloribus esse ratione nulla tenetur commodi et illo! Doloremque labore expedita dolorem aliquam animi magni est minima, laudantium temporibus placeat facilis recusandae consequatur asperiores eaque ratione fugit cumque iure reiciendis nostrum. Qui totam magni quae saepe. Similique optio aut officiis voluptatem perferendis odio numquam, modi ipsa autem laboriosam!</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
