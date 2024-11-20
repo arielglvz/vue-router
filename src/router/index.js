7b873f0a7a55a2476582d2707faa54f418b5af3a
arielglvz <arielglvz@gmail.com>
2024-11-20 19:47:39 +0800

    Redirect & CatchAll 404

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   {
@@ -26,7 +27,18 @@ const routes = [
     name: 'JobDetails',
     component: JobDetails,
     props: true
-  }, 
+  },
+  // redirect
+  {
+    path: '/all-jobs',
+    redirect: '/jobs'
+  },
+  // CatchAll 404
+  {
+    path: "/:catchAll(.*)",
+    name: 'NotFound',
+    component: NotFound
+  }
 ]
 
 const router = createRouter({
