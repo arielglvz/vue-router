b6e8c457e0337a183b83d7ba7d496f6fd880a316
arielglvz <arielglvz@gmail.com>
2024-11-21 00:54:30 +0800

    Added json-server for mock db and fetch to get the data

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,24 +1,32 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
-      <h2>{{ job.title }}</h2>
-    </router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
+        <h2>{{ job.title }}</h2>
+      </router-link>
+    </div>
+  </div>
+  <div v-else>
+    <p>Loading jobs...</p>
   </div>
 </template>
 
 <script>
+// const URL = 'http://localhost:3000/jobs'
 export default {
   data() {
     return {
-      jobs: [
-        { title: 'Ninja UX Designer', id: 1, details: 'lorem' },
-        { title: 'Ninja Web Developer', id: 2, details: 'lorem' },
-        { title: 'Ninja Vue Designer', id: 3, details: 'lorem' },
-      ]
+      jobs: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/jobs')
+    .then(res => res.json())
+    .then(data => this.jobs = data)
+    .catch(err => console.log('error: ', err.message))
     }
   }
-}
 </script>
 
 <style>
